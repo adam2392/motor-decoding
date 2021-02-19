08d63e755bef473bcd6d519ac6bab5d6a44812df
ChesterHuynh <chester.huynh924@gmail.com>
2021-02-19 03:57:40 -0500

    Add results notebooks for efri02,06,07

--- mtsmorf/move_exp/plotting.py
+++ mtsmorf/move_exp/plotting.py
@@ -15,6 +15,7 @@ from sklearn.metrics import roc_curve, auc
 from sklearn.preprocessing import label_binarize
 from sklearn.utils import check_random_state
 import dabest
+from mne.time_frequency.tfr import AverageTFR, EpochsTFR
 
 label_names = {0: "Down", 1: "Right", 2: "Up", 3: "Left"}
 colors = cycle(["#26A7FF", "#7828FD", "#FF5126", "#FDF028"])
@@ -52,62 +53,158 @@ def _plot_signal(t, data, title="", ax=None, label="", ls="-", **plt_kwargs):
     return ax
 
 
-def plot_signals(epochs, labels, ncols=4, axs=None):
-    nchs = len(epochs.ch_names)
-    t = epochs.times
+def plot_signals(epochs, labels, picks=None, axs=None):
+    nchs = len(epochs)
+    times = epochs.times
+
+    if picks is None:
+        picks = epochs.ch_names
 
     if axs is None:
-        fig, axs = plt.subplots(
-            dpi=200, nrows=int(np.ceil(nchs / ncols)), ncols=ncols, figsize=(15, 45)
-        )
+        nrows = int(np.ceil(len(picks) / 4))
+        ncols = min(len(picks), 4)
+        width = 8 * ncols
+        height = 5 * nrows
+        _, axs = plt.subplots(nrows, ncols, dpi=200, figsize=(width, height))
+        axs = axs.flatten()
 
-    for i, ch in enumerate(epochs.ch_names):
+    epochs_data = epochs.get_data(picks)
+    for i, ch in enumerate(picks):
         ax = axs[i]
-
-        epochs_data = epochs.get_data()
-        data = epochs_data[:, i]
+        ch_data = epochs_data[:, i] / 1e6
 
         # for each class label
-        for j, (label, color) in enumerate(zip(np.unique(labels), colors)):
-            if label in [1, 3]:
-                continue
+        for label, color in zip(np.unique(labels), colors):
 
             _plot_signal(
-                t,
-                data[labels == label],
+                times,
+                ch_data[labels == label],
                 ax=ax,
                 label=f"{label_names[label]}",
                 color=color,
             )
 
-            ax.legend()
-            ax.set(title=f"{ch}", xlabel="Time (s)", ylabel="LFP (mV)")
+            ax.legend(fontsize=10, ncol=2, loc="lower right")
+            ax.set(title=f"{ch}", xlabel="Time (s)", ylabel="mV")
 
     return axs
 
 
-def _compute_roc_multiclass(y_true, y_score, n_classes):
+def plot_epoch_time_series(epochs, picks=None, vmin=None, vmax=None, axs=None):
+    """Plot heatmap similar to mne.viz.plot_epochs.
 
-    fprs = []
-    tprs = []
-    roc_auc = []
+    Args:
+        epochs (mne.Epochs): Epochs instance
+        picks (list, optional): List of channel names from epochs to use. Defaults to None.
+        vmin (int or list, optional): Smallest color value for each heatmap. Defaults to None.
+        vmax (int or list, optional): Largest color value for each heatmap. Defaults to None.
+        axs (matplotlib.axes.Axes, optional): Axes to plot. Defaults to None.
+    """
+    if isinstance(vmin, (int, float)):
+        vmins = [vmin for _ in range(len(picks))]
 
-    for i in range(n_classes):
-        mean_fpr = np.linspace(0, 1, 100)
+    if isinstance(vmax, (int, float)):
+        vmaxs = [vmax for _ in range(len(picks))]
 
-        fpr, tpr, _ = roc_curve(
-            label_binarize(y_true, classes=np.arange(n_classes))[:, i],
-            y_score[:, i],
+    if picks is None:
+        picks = epochs.ch_names
+
+    if axs is None:
+        nrows = len(picks)
+        width = 6
+        height = 3 * nrows
+        _, axs = plt.subplots(nrows, figsize=(width, height), dpi=200)
+
+    picks_data = epochs.get_data(picks).transpose(1, 0, 2) / 1e6
+    ntrials = picks_data.shape[1]
+    for ax, pick, data, vmin_, vmax_ in zip(axs, picks, picks_data, vmins, vmaxs):
+        sns.heatmap(
+            data,
+            cmap=plt.cm.coolwarm,
+            ax=ax,
+            vmin=vmin_,
+            vmax=vmax_,
+            center=0.0,
+            cbar_kws={"label": "mV"}
         )
+        ax.invert_yaxis()
 
-        interp_tpr = np.interp(mean_fpr, fpr, tpr)
-        interp_tpr[0] = 0.0
+        times = epochs.times
+        time_lock = np.argmin(np.abs(times))
+        xticks = [0, time_lock, len(times) - 1]
+        xticklabels = [f"{label:.1f}" for label in times[xticks]]
+        yticks = np.arange(0, ntrials, 25)
+        yticklabels = yticks + 1
 
-        fprs.append(mean_fpr)
-        tprs.append(interp_tpr)
-        roc_auc.append(auc(fprs[i], tprs[i]))
+        ax.axvline(x=time_lock, ls="--", c="r", label="'Left Center' onset")
+        ax.legend(
+            bbox_to_anchor=[0.79, 1], loc="lower center", fontsize=10, frameon=True
+        )
+        ax.set(
+            xlabel="Time (s)",
+            ylabel="Trials",
+            title=pick,
+            xticks=xticks,
+            yticks=yticks,
+        )
+        ax.set_xticklabels(xticklabels, rotation=0),
+        ax.set_yticklabels(yticklabels, rotation=0)
+    pass
 
-    return fprs, tprs, roc_auc
+
+def plot_spectrogram(power, freqs, n_cycles, picks, func="average", db=False, axs=None):
+    if picks is None:
+        picks = power.ch_names
+
+    if axs is None:
+        nrows = int(np.ceil(len(picks) / 4))
+        ncols = min(len(picks), 4)
+        width = 8 * ncols
+        height = 5 * nrows
+        _, axs = plt.subplots(nrows, ncols, dpi=100, figsize=(width, height))
+        axs = axs.flatten()
+
+    if not isinstance(power, AverageTFR):
+        if func == "average":
+            avgpower = power.average()
+            avgpower_data = avgpower.data
+        elif func == "median":
+            avgpower_data = np.median(power.data / 1e6, axis=0)
+    elif isinstance(power, EpochsTFR):
+        avgpower_data = power.data
+    else:
+        raise TypeError("power must be either an EpochsTFR or AverageTFR instance.")
+
+    ch_names = power.ch_names
+    avgpower_data = avgpower.data / 1e6
+
+    times = power.times
+    time_lock = np.argmin(np.abs(times))
+    xticks = [0, time_lock, len(times) - 1]
+    xticklabels = [f"{label:.1f}" for label in times[xticks]]
+    yticks = np.arange(0, len(freqs), 2)
+    yticklabels = [f"{freq:.1f}" for freq in freqs[yticks]]
+
+    for ax, pick in zip(axs, picks):
+        ind = ch_names.index(pick)
+        data = avgpower_data[ind]
+        if db:
+            data = np.log10(data)
+        sns.heatmap(data, cmap="viridis", cbar_kws={"label": "dB"}, ax=ax)
+        ax.invert_yaxis()
+        ax.axvline(x=time_lock, ls="--", c="r", label="'Left Center' onset")
+        ax.legend(
+            bbox_to_anchor=[0.79, 1], loc="lower center", fontsize=10, frameon=True
+        )
+        ax.set(
+            xlabel="Time (s)",
+            ylabel="Frequency (Hz)",
+            title=pick,
+            xticks=xticks,
+            yticks=yticks,
+        )
+        ax.set_xticklabels(xticklabels, rotation=0),
+        ax.set_yticklabels(yticklabels, rotation=0)
 
 
 def plot_feature_importances(
@@ -130,7 +227,7 @@ def plot_feature_importances(
 
     if vmax is None:
         vmax = np.max(feat_importance_means)
-    
+
     ax = sns.heatmap(
         df_feat_importances,
         vmin=vmin,
@@ -228,7 +325,8 @@ def plot_roc_cv(y_pred_probas, X, y, test_inds, label="", show_chance=True, ax=N
     ax.plot(
         mean_fpr,
         mean_tpr,
-        label=label + r"Mean ROC (AUC = {mean_auc:.3f} $\pm$ {std_auc:.3f})".format(
+        label=label
+        + r"Mean ROC (AUC = {mean_auc:.3f} $\pm$ {std_auc:.3f})".format(
             mean_auc=mean_auc, std_auc=std_auc
         ),
         ls="-",
@@ -245,12 +343,40 @@ def plot_roc_cv(y_pred_probas, X, y, test_inds, label="", show_chance=True, ax=N
     )
 
     if show_chance:
-        ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Chance", alpha=0.8)
+        ax.plot(
+            [0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Chance", alpha=0.8
+        )
 
     return ax
 
 
-def plot_roc_multiclass_cv(y_pred_probas, X, y, test_inds, label="", show_chance=True, ax=None):
+def _compute_roc_multiclass(y_true, y_score, n_classes):
+
+    fprs = []
+    tprs = []
+    roc_auc = []
+
+    for i in range(n_classes):
+        mean_fpr = np.linspace(0, 1, 100)
+
+        fpr, tpr, _ = roc_curve(
+            label_binarize(y_true, classes=np.arange(n_classes))[:, i],
+            y_score[:, i],
+        )
+
+        interp_tpr = np.interp(mean_fpr, fpr, tpr)
+        interp_tpr[0] = 0.0
+
+        fprs.append(mean_fpr)
+        tprs.append(interp_tpr)
+        roc_auc.append(auc(fprs[i], tprs[i]))
+
+    return fprs, tprs, roc_auc
+
+
+def plot_roc_multiclass_cv(
+    y_pred_probas, X, y, test_inds, label="", show_chance=True, ax=None
+):
 
     if ax is None:
         _, ax = plt.subplots()
@@ -315,7 +441,10 @@ def plot_roc_multiclass_cv(y_pred_probas, X, y, test_inds, label="", show_chance
             mean_fpr,
             mean_tpr,
             label=r"{label_name}: {clf_name} Mean ROC (AUC = {mean_auc:.3f} $\pm$ {std_auc:.3f})".format(
-                label_name=label_names[i], clf_name=label, mean_auc=mean_auc, std_auc=std_auc
+                label_name=label_names[i],
+                clf_name=label,
+                mean_auc=mean_auc,
+                std_auc=std_auc,
             ),
             ls="-",
         )
@@ -331,7 +460,9 @@ def plot_roc_multiclass_cv(y_pred_probas, X, y, test_inds, label="", show_chance
         )
 
     if show_chance:
-        ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Chance", alpha=0.8)
+        ax.plot(
+            [0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Chance", alpha=0.8
+        )
 
     ax.set(
         xlabel="False Positive Rate",
@@ -409,46 +540,55 @@ def plot_accuracies(clf_scores, ax=None, random_seed=1):
     if ax is None:
         _, ax = plt.subplots()
 
-    id_col = pd.Series(range(1, len(clf_scores)+1))
+    id_col = pd.Series(range(1, len(clf_scores) + 1))
 
-    df = pd.DataFrame({
-        clf_name: scores["test_accuracy"] 
-        for clf_name, scores in clf_scores.items()
-    })
+    df = pd.DataFrame(
+        {clf_name: scores["test_accuracy"] for clf_name, scores in clf_scores.items()}
+    )
 
     df["ID"] = id_col
 
-    my_data = dabest.load(df, idx=list(clf_scores.keys()), 
-                        id_col="ID", resamples=100, 
-                        random_seed=random_seed)
+    my_data = dabest.load(
+        df,
+        idx=list(clf_scores.keys()),
+        id_col="ID",
+        resamples=100,
+        random_seed=random_seed,
+    )
 
     my_data.mean_diff.plot(ax=ax)
     ax.set(
         title="Classifier Accuracy Comparison",
-    );
+    )
     return ax
 
 
 def plot_roc_aucs(clf_scores, ax=None, random_seed=1):
-    
+
     if ax is None:
         _, ax = plt.subplots()
 
-    id_col = pd.Series(range(1, len(clf_scores)+1))
+    id_col = pd.Series(range(1, len(clf_scores) + 1))
 
-    df = pd.DataFrame({
-        clf_name: scores["test_roc_auc_ovr"] 
-        for clf_name, scores in clf_scores.items()
-    })
+    df = pd.DataFrame(
+        {
+            clf_name: scores["test_roc_auc_ovr"]
+            for clf_name, scores in clf_scores.items()
+        }
+    )
 
     df["ID"] = id_col
 
-    my_data = dabest.load(df, idx=list(clf_scores.keys()), 
-                        id_col="ID", resamples=100, 
-                        random_seed=random_seed)
+    my_data = dabest.load(
+        df,
+        idx=list(clf_scores.keys()),
+        id_col="ID",
+        resamples=100,
+        random_seed=random_seed,
+    )
 
     my_data.mean_diff.plot(ax=ax)
-    ax.set(title="Classifier ROC AUC One vs. Rest Comparison");
+    ax.set(title="Classifier ROC AUC One vs. Rest Comparison")
     return ax
 
 
@@ -456,36 +596,37 @@ def plot_classifier_performance(clf_scores, X, y, axs=None):
 
     if axs is None:
         axs = plt.subplots(ncols=2)
+        axs = axs.flatten()
 
     if len(axs) != 2:
         raise ValueError("Axes to be plotted to should have exactly 2 subplots.")
-    
-    axs = axs.flatten()
-    
+
+
     # 1. Plot roc curves
-    n_classes = len(np.unique(y))
-    if n_classes > 2:
-        for clf_name, scores in clf_scores.items():
-            plot_roc_multiclass_cv(
-                scores["test_predict_proba"],
-                X,
-                y,
-                scores["test_inds"],
-                label=clf_name,
-                show_chance=False,
-                ax=axs[0],
-            )
-    else:
-        for clf_name, scores in clf_scores.items():
-            plot_roc_cv(
-                scores["test_predict_proba"],
-                X,
-                y,
-                scores["test_inds"],
-                label=clf_name,
-                show_chance=False,
-                ax=axs[0],
-            )
+    # n_classes = len(np.unique(y))
+    # if n_classes > 2:
+    #     for clf_name, scores in clf_scores.items():
+    #         plot_roc_multiclass_cv(
+    #             scores["test_predict_proba"],
+    #             X,
+    #             y,
+    #             scores["test_inds"],
+    #             label=clf_name,
+    #             show_chance=False,
+    #             ax=axs[0],
+    #         )
+    # else:
+    #     for clf_name, scores in clf_scores.items():
+    #         plot_roc_aucs(
+    #             scores["test_predict_proba"],
+    #             X,
+    #             y,
+    #             scores["test_inds"],
+    #             label=clf_name,
+    #             show_chance=False,
+    #             ax=axs[0],
+    #         )
+    plot_roc_aucs(clf_scores, ax=axs[0])
 
     # 2. Plot accuracies
     plot_accuracies(clf_scores, ax=axs[1])
